4c9853fe8e9b67956a88318a8c780661fab64c7e
ArthurGarraud <arthurgarraud@proton.me>
2025-10-19 21:53:08 +0100

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.html", "./src/**/*.js"],
-  theme: { extend: {} },
+  theme: {
+      extend: {
+      }
+  },
   plugins: [],
 }
